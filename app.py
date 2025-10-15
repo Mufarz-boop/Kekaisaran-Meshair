f6c0e6dec7bcdcbc65ea4a70f6806450899eb3c7
Mufarz-boop <kaeldrakvor@gmail.com>
2025-10-15 08:43:30 +0700

    Qullaz

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_pemilu_osis'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -18,6 +24,15 @@ def login():
 @app.route('/pemilu')
 def pemilu():
     return render_template('pemilu/index.html')
+
+@app.route('/kelas')
+def kelas():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM kelas")
+    data = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('kelas/index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
